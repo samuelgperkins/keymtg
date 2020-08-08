032be66317a51d366ff0a0af0ae2c4464537c56a
sperkins <sperkins@gmail.com>
2020-08-07 22:52:15 -0700

    Updates

---
+++ precache-manifest.bde4b371d4bce006fcba41ab075bd7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c7d7979b7823a415364ca16768cafd",
+    "url": "/keymtg/index.html"
+  },
+  {
+    "revision": "f2ad41d4228f92896c90",
+    "url": "/keymtg/static/css/main.546f271c.chunk.css"
+  },
+  {
+    "revision": "67ecc2d71dfaac0949c6",
+    "url": "/keymtg/static/js/2.f48c97e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keymtg/static/js/2.f48c97e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ad41d4228f92896c90",
+    "url": "/keymtg/static/js/main.33490c4c.chunk.js"
+  },
+  {
+    "revision": "f872173a6697edc10d07",
+    "url": "/keymtg/static/js/runtime-main.c615591f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/keymtg/public/precache-manifest.2e9046fe15b72d2e425a8d53634523e2.js"
+  "/keymtg/precache-manifest.bde4b371d4bce006fcba41ab075bd7cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keymtg/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keymtg/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
